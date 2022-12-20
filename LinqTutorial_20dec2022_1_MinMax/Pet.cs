2b7f692022ab5b9006936c9cd8dbe165e8e4fe14
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-20 02:28:25 +0100

    Add project files.

---
+++ LinqTutorial_20dec2022_1_MinMax/Pet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTutorial_20dec2022_1_MinMax;
+
+public enum PetType
+{
+    Dog,
+    Cat,
+    Fish
+}
+
+public class PetByTypeComparer : IComparer<Pet>
+{
+    int IComparer<Pet>.Compare(Pet? x, Pet? y)
+    {
+        return x.Type.CompareTo(y.Type);
+    }
+}
+public class PetComparerById : IEqualityComparer<Pet>
+{
+    bool IEqualityComparer<Pet>.Equals(Pet? x, Pet? y)
+    {
+        return x.Id == y.Id;
+    }
+
+    int IEqualityComparer<Pet>.GetHashCode(Pet obj)
+    {
+        return obj.Id;
+    }
+}
+internal class Pet : IComparable<Pet>
+{
+    public Pet(int id, string name, PetType type, float weight)
+    {
+        Id = id;
+        Name = name;
+        Type = type;
+        Weight = weight;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public PetType Type { get; set; }
+    public float Weight { get; set; }
+
+    public int CompareTo(Pet? other)
+    {
+        return Weight.CompareTo(other.Weight);
+    }
+}
---
+++ LinqTutorial_20dec2022_1_MinMax/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LinqTutorial_20dec2022_1_MinMax;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var pets = new[]
+    {
+        new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+        new Pet(2, "Anthony", PetType.Cat, 2f),
+        new Pet(3, "Ed", PetType.Cat, 0.7f),
+        new Pet(4, "Taiga", PetType.Dog, 35f),
+        new Pet(5, "Rex", PetType.Dog, 40f),
+        new Pet(6, "Lucky", PetType.Dog, 5f),
+        new Pet(7, "Storm", PetType.Cat, 0.9f),
+        new Pet(8, "Nyan", PetType.Cat, 2.2f)
+    };
+        var minWeight = pets.Min(pet => pet.Weight);
+        Console.WriteLine($"{nameof(minWeight)}: {minWeight}");
+        var maxWeight = pets.Max(pet => pet.Weight);
+        Console.WriteLine($"{nameof(maxWeight)}: {maxWeight}");
+
+        var minPet = pets.Min();
+        Console.WriteLine($"{nameof(minPet)}: {minPet.Name}, {minPet.Weight}");
+
+        var emptyNumbers = new int[0];
+        var minimalNumber = emptyNumbers.Min(); /*collection must not be empty!*/
+
+        Console.WriteLine("\n");
+
+        var numbers = new[] { 16, 8, 9, -1, 2 };
+        var smallest = numbers.Min(number => number);
+        Console.WriteLine($"{smallest}");
+
+        var largest = numbers.Max(number => number);
+        Console.WriteLine($"{largest}");
+
+        var largest2 = numbers.Max();
+        Console.WriteLine($"{largest2}");
+
+    }
+}
